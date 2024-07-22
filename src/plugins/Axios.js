c165711342534cc1cc665362c8b45be3da711260
nathalie.soto <nathalie.soto@le-campus-numerique.fr>
2024-07-22 10:35:18 +0200

    added proxy conf

--- src/plugins/Axios.js
+++ src/plugins/Axios.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api', // Base URL of your Laravel API
+    baseURL: 'http://127.0.0.1:8081/api', // Base URL of your Laravel API
     withCredentials: true, // Allow sending cookies along with the request (necessary for CSRF cookie)
 });
 
 // Request CSRF cookie at the beginning of client connection
-axiosInstance.get('http://127.0.0.1:8000/sanctum/csrf-cookie').then(response => {
+axiosInstance.get('http://127.0.0.1:8081/sanctum/csrf-cookie').then(response => {
     // Successfully obtained CSRF cookie
     console.log('CSRF cookie obtained:', response.status);
 }).catch(error => {
